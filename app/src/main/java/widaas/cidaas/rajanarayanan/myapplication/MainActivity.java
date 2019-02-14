4548a09113a97758197cb0ee4e086933edc9ad0a
Cidaas <developer@cidaas.de>
2019-02-14 15:33:19 +0530

    no mess

--- app/src/main/java/widaas/cidaas/rajanarayanan/myapplication/MainActivity.java
+++ app/src/main/java/widaas/cidaas/rajanarayanan/myapplication/MainActivity.java
@@ -1,26 +1,101 @@
 package widaas.cidaas.rajanarayanan.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import widaas.cidaas.rajanarayanan.myandroidlibrary.AndroidCidaas;
+import widaas.cidaas.rajanarayanan.myandroidlibrary.Biometric.BiometricCallback;
+import widaas.cidaas.rajanarayanan.myandroidlibrary.Biometric.BiometricManager;
 import widaas.cidaas.rajanarayanan.mylibrary.SampleTesting;
 
 public class MainActivity extends AppCompatActivity {
-
+    AndroidCidaas androidCidaas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SampleTesting sampleTesting=new SampleTesting();
-        AndroidCidaas androidCidaas=new AndroidCidaas();
+        androidCidaas=new AndroidCidaas(this);
 
-        Toast.makeText(this, ""+ sampleTesting.getName(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, ""+ sampleTesting.getName(), Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(this, ""+androidCidaas.getGetClass(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, ""+androidCidaas.getGetClass(), Toast.LENGTH_SHORT).show();
     }
 
 
+    public void buttonClick(View view)
+    {
+
+       //  androidCidaas.getLoginDetails();
+
+        new BiometricManager.BiometricBuilder(MainActivity.this)
+                .setTitle(getString(R.string.biometric_title))
+                .setSubtitle(getString(R.string.biometric_subtitle))
+                .setDescription(getString(R.string.biometric_description))
+                .setNegativeButtonText(getString(R.string.biometric_negative_button_text))
+                .build()
+                .authenticate(new BiometricCallback() {
+                    @Override
+                    public void onSdkVersionNotSupported() {
+                        Toast.makeText(MainActivity.this, "SDK not supported", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onBiometricAuthenticationNotSupported() {
+                        Toast.makeText(MainActivity.this, "onBiometricAuthenticationNotSupported", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onBiometricAuthenticationNotAvailable() {
+                        Toast.makeText(MainActivity.this, "onBiometricAuthenticationNotAvailable", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onBiometricAuthenticationPermissionNotGranted() {
+                        Toast.makeText(MainActivity.this, "onBiometricAuthenticationPermissionNotGranted", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onBiometricAuthenticationInternalError(String error) {
+                        Toast.makeText(MainActivity.this, "onBiometricAuthenticationInternalError", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationFailed() {
+                        Toast.makeText(MainActivity.this, "onAuthenticationFailed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationCancelled() {
+                        Toast.makeText(MainActivity.this, "onAuthenticationCancelled", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationSuccessful() {
+                        Toast.makeText(MainActivity.this, "onAuthenticationSuccessful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+                        Toast.makeText(MainActivity.this, "onAuthenticationHelp", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationError(int errorCode, CharSequence errString) {
+                        Toast.makeText(MainActivity.this, "onAuthenticationError", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        androidCidaas.onActivityResult(requestCode,resultCode,data);
+    }
 }
--- myandroidlibrary/src/main/java/widaas/cidaas/rajanarayanan/myandroidlibrary/AndroidCidaas.java
+++ myandroidlibrary/src/main/java/widaas/cidaas/rajanarayanan/myandroidlibrary/AndroidCidaas.java
@@ -1,12 +1,105 @@
 package widaas.cidaas.rajanarayanan.myandroidlibrary;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import static android.content.ContentValues.TAG;
+
 public class AndroidCidaas {
 
+    private static final int RC_SIGN_IN = 100;
     String getClass;
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+
+    Activity activityGlobal;
 
     public String getGetClass()
     {
         getClass="Cidaas Widas";
         return getClass;
     }
+
+
+    public AndroidCidaas(Activity activity)
+    {
+
+      gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+      activityGlobal=activity;
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(activity, gso);
+
+
+    }
+
+
+    public  void  getLoginDetails()
+    {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(activityGlobal);
+
+        if(account!=null)
+        {
+            Toast.makeText(activityGlobal, "succcess"+account.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+
+
+            signIn();
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        activityGlobal.startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(activityGlobal, "succcess"+account.getDisplayName(), Toast.LENGTH_SHORT).show();
+           // updateUI(account);
+        } catch (ApiException e) {
+
+            Toast.makeText(activityGlobal, "Error", Toast.LENGTH_SHORT).show();
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+
+
 }
